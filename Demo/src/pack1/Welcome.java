85bdd78ba5cbd7fc5992c6386cfef29cd7f22932
harika0330 <142019124+harika0330@users.noreply.github.com>
2023-10-30 12:07:18 +0530

    Update Welcome.java

--- Demo/src/pack1/Welcome.java
+++ Demo/src/pack1/Welcome.java
@@ -4,7 +4,8 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GPREC");	
-		System.out.println("Hello");// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("World");
 
 	}
 
