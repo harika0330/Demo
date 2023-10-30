e0c0784a4b268fad73f1dcf45d03a7cbc2b6833d
Harika <kharikahari488@gmail.com>
2023-10-30 11:57:52 +0530

    second commit

--- Demo/src/pack1/Welcome.java
+++ Demo/src/pack1/Welcome.java
@@ -3,7 +3,8 @@ package pack1;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to GPREC");		// TODO Auto-generated method stub
+		System.out.println("Welcome to GPREC");	
+		System.out.println("Hello");// TODO Auto-generated method stub
 
 	}
 
